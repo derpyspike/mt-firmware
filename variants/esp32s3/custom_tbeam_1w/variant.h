dcc6e3a18db9fe92ae35041605e53e67708eabda
derpyspike <git@derps.me>
2025-12-16 16:36:45 +0100

    Improvements to the TBeam 1W variant with inspiration from the upstream PR. 	* Now with the PA ramp implemented 	* Battery Voltage PIN fixed 	* variant.cpp no longer used

--- variants/esp32s3/custom_tbeam_1w/variant.h
+++ variants/esp32s3/custom_tbeam_1w/variant.h
@@ -17,8 +17,23 @@
 #define LORA_DIO3   -1  
 
 // Critical Radio Power Settings
-#define PIN_LORA_POWER 40
+//#define PIN_LORA_POWER 40
 #define LORA_RXEN      21
+// CRITICAL: Radio power enable - MUST be HIGH before lora.begin()!
+// GPIO 40 powers the SX1262 + PA module via LDO
+#define SX126X_POWER_EN 40
+
+// SPI (shared by LoRa and SD)
+#define SPI_MOSI 11
+#define SPI_SCK 13
+#define SPI_MISO 12
+#define SPI_CS 10
+
+
+// PA Ramp Time - T-Beam 1W requires >800us stabilization (default is 200us)
+// Value 0x05 = RADIOLIB_SX126X_PA_RAMP_800U
+#define SX126X_PA_RAMP_US 0x05
+
 
 // TCXO Voltage: 3.0V required as per the Lilygo doc. 
 #define SX126X_DIO3_TCXO_VOLTAGE 3.0
@@ -33,6 +48,11 @@
 // --- Power Management (AXP2101) (I disabled this because it causes Critical fault 6) (seems to work either way) ---
 //#define HAS_AXP2101
 
+// Battery ADC
+#define BATTERY_PIN 4
+#define ADC_CHANNEL ADC1_GPIO4_CHANNEL
+#define BATTERY_SENSE_SAMPLES 30
+#define ADC_MULTIPLIER 2.9333
 
 // FIX: Pin 4 is Battery ADC, not IRQ. Disable IRQ to force polling.
 //#define PMU_IRQ         -1   
@@ -53,14 +73,24 @@
 // --- SD Card (I don't think it has any use but it's a thing) ---
 #define HAS_SDCARD
 #define SDCARD_CS       10
-#define SDCARD_MOSI     11
-#define SDCARD_MISO     12 
-#define SDCARD_SCK      13 
+//#define SDCARD_MOSI     11
+//#define SDCARD_MISO     12 
+//#define SDCARD_SCK      13 
+#define SDCARD_USE_SPI1
+#define SDCARD_CS SPI_CS
+
 
 // --- Buttons & LED ---
 #define BUTTON_PIN      0   
 #define BUTTON_PIN_ALT  17  
 #define LED_PIN         18  
-#define LED_STATE_ON    1   
+#define LED_STATE_ON    1
+
+
+
+#define NTC_PIN 14
+
+// 32768 Hz crystal present
+#define HAS_32768HZ 1
 
 #endif
